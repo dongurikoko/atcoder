29f65811e67c56edc618affbd0a9572aa5223712
dongurikoko <sio1357908642@outlook.jp>
2024-04-10 03:03:24 +0900

    348実装

--- ABC/348/a.cpp
+++ ABC/348/a.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        if(i % 3 == 0){
+            cout << "x";
+        }
+        else{
+            cout << "o";
+        }
+    }
+    return 0;
 }
